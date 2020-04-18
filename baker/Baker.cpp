4a8d829ac27cb2cbda61fc7a68a6cfe27a4e4819
kevin <kevin@ubuntu>
2020-04-17 21:25:15 -0700

    Went alone on this project. Got most of everything so far from Producer_Many_Consumer. Finally finished Baker and Waiter. Hopefully it works. Now I will begin working on Logger.

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -1,7 +1,19 @@
 #include <mutex>
 
 #include "../includes/baker.h"
+#include "../includes/externs.h"
+
 using namespace std;
+
+//consumer
+//mutex the queue, waiter cannot access same time as baker, vise versa
+//when adding and removing from queue, use unique lock
+//more meaty and difficult order
+
+//if queue empty, stop >> Need to use condition variable (cv.wait()) >> cv.wait(lck) releases mutex lock
+//else, get from queue
+
+//while queue is empty AND waiter not done
 
 Baker::Baker(int id):id(id)
 {
@@ -11,10 +23,78 @@ Baker::~Baker()
 {
 }
 
+//bake, box and append to anOrder.boxes vector
+//if order has 13 donuts there should be 2 boxes
+//1 with 12 donuts, 1 with 1 donut
 void Baker::bake_and_box(ORDER &anOrder) {
-	
+
+	int needed_boxes = anOrder.number_donuts / 12;
+	int extra_donuts = anOrder.number_donuts % 12;
+
+	if (extra_donuts != 0) {
+		needed_boxes++;
+	}
+
+	int leftover_donuts = anOrder.number_donuts;
+
+	for(int i = 0; i < needed_boxes; i++) {
+		if (leftover_donuts > 11) {
+			Box aBox;
+
+			for (int j = 0; j < 12; j++) {
+				leftover_donuts--;
+				DONUT aDonut;
+				aBox.addDonut(aDonut);
+			}
+			anOrder.boxes.push_back(aBox);
+		}
+	}
 }
 
 void Baker::beBaker() {
 	
+	while (!b_WaiterIsFinished) {
+
+		{
+			unique_lock<mutex> lck(mutex_order_inQ);
+			cv_order_inQ.wait(lck);
+		}
+
+		ORDER ord;
+
+		{
+			lock_guard<mutex> lck(mutex_order_inQ);
+
+			ord = order_in_Q.front();
+			order_in_Q.pop();
+		}
+
+		this -> bake_and_box(ord);
+
+		{
+			lock_guard<mutex> lck(mutex_order_outQ);
+			order_out_Vector.push_back(ord);
+		}
+
+		while (!order_in_Q.empty()) {
+
+			ORDER ord;
+
+			{
+				lock_guard<mutex> lck(mutex_order_inQ);
+
+				ord = order_in_Q.front();
+				order_in_Q.pop();
+			}
+
+			this -> bake_and_box(ord);
+
+			{
+				lock_guard<mutex> lck(mutex_order_outQ);
+				order_out_Vector.push_back(ord);
+			}
+		}
+
+	}
+
 }
--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -2,21 +2,55 @@
 #include "stdlib.h"
 
 #include "../includes/waiter.h"
+#include "../includes/externs.h"
 
 using namespace std;
+//producer
+//order grabs lock, puts order into vector, unlocks
+//gcount++ >> add to vector
+//exclusive access to adding order. not as difficult
+//cv,notify vs cv.notifyall
 
-Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){
+//cv.notify normally,
+//when everything is done, cv.notifyall to make everything shut down
+
+//when woken up
+//if in vector do job
+
+//if woken up and nothing in queue, saying done.
+
+Waiter::Waiter(int id,std::string filename):id(id),myIO(filename) {
 }
 
-Waiter::~Waiter()
-{
+Waiter::~Waiter() {
 }
+
+
+
 
 //gets next Order(s) from file_IO
 int Waiter::getNext(ORDER &anOrder){
-	return SUCCESS;
+	return myIO.getNext(anOrder);
 }
 
 void Waiter::beWaiter() {
+
+	while (true) {
+		ORDER ord;
+
+		int areThereMoreOrders = this -> getNext(ord);
+		if (areThereMoreOrders != SUCCESS) {
+			b_WaiterIsFinished = true;
+			break;
+		}
+
+		{
+			lock_guard<mutex> lck(mutex_order_inQ);
+			order_in_Q.push(ord);
+		}
+
+		cv_order_inQ.notify_one();
+	}
+	cv_order_inQ.notify_all();
 }
 
